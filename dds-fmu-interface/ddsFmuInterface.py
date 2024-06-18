55dc34c9268565cb08cca569082bcee4527a17b7
Edvard Sire <mail@edvardsire.com>
2024-06-18 22:00:36 +0200

    first test with rename

--- dds-fmu-interface/ddsFmuInterface.py
+++ dds-fmu-interface/ddsFmuInterface.py
@@ -3,7 +3,7 @@ from cyclonedds.pub import DataWriter
 from cyclonedds.sub import DataReader
 from cyclonedds.topic import Topic
 
-from _idl._dds_fmu import MyCustomSignalStructureInput, MyCustomSignalStructureOutput, thrusterBowStruct, thrusterPortStruct, thrusterStarboardStruct, hullStruct
+from _idl._dds_fmu import DDSInput, DDSOutput
 
 
 
@@ -14,28 +14,16 @@ from _idl._dds_fmu import MyCustomSignalStructureInput, MyCustomSignalStructureO
 # For our usage we simply leave parameters as default, nothing to set up basically
 dp = DomainParticipant()
 
-# This is Where we define topics and what type of data structure they have
-topicInput =  Topic(dp, "MyCustomDDSFMUTopicInput", MyCustomSignalStructureInput)
-topicOutput = Topic(dp, "MyCustomDDSFMUTopicOutput", MyCustomSignalStructureOutput)
-
-# We now finally bind these topics to DDS with correct data structure
-# This is the objects we will use to read or write to our DDS-FMU on STC
-dr = DataReader(dp, topicInput)
-dw = DataWriter(dp, topicOutput)
-# DDS Topic Setup (STOP) --------------------------------------------------
+# reader = DataReader(dp, Topic(dp, "DDSInput", DDSInput))
+# writer = DataWriter(dp, Topic(dp, "DDSOutput", DDSOutput))
+writer = DataWriter(dp, Topic(dp, "MyCustomDDSFMUTopicInput", DDSInput))
 
 
-
-# DDS Functions (START) --------------------------------------------------
-# Function to read data from DDS
 def read_data():
-    # Read value from DDS-FMU
-    msg = dr.read()
+    msg = reader.read()
     print(msg)
 
-    # Check that the message is not empty and that DDS-FMU actually published something
     if msg:
-        # Message is a array, and inside the array we find our data structure with aropriate data
         data = msg[0].fmu_output_value
         
         return data
@@ -45,8 +33,7 @@ def read_data():
     
 # Function to publish data to DDS
 def publish_data() -> None:
-    # Construct the message with the correct data type and values inside to publish to DDS, so that DDS FMU can receive it and output it 
-    # msg = MyCustomSignalStructureOutput( outputHullPosition= 6*[1.0],
+    # msg = DDSOutput( outputHullPosition= 6*[1.0],
     #     outputAntenna1Position= 3*[2.0],
     #     outputAntenna2Position= 3*[3.0],
     #     ouputVelocitySpeed= 4.0,
@@ -54,33 +41,26 @@ def publish_data() -> None:
     #     outputAccelerationLinear = 3*[6.0],
     #     outputVelocityAngular = 3*[7.0]
     #     )
-    msg = MyCustomSignalStructureInput(
-            thrusterBowStruct(2.0, 2.0),
-            thrusterPortStruct(2.0, 2.0),
-            thrusterStarboardStruct(2.0, 2.0),
-            hullStruct(1.0, 2.0, 3.0, 4.0, 5.0)
-        )
+
+    msg = DDSInput(
+            TBinputForce = 2.0,
+            TBinputAngle = 2.0,
+            TPinputForce = 2.0,
+            TPinputAngle = 2.0,
+            TSinputForce = 2.0,
+            TSinputAngle = 2.0,
+            inputWindSpeed = 2.0,
+            inputWindAngle = 2.0,
+            inputWaterCurrentSpeed = 2.0,
+            inputWaterCurrentAngle = 2.0,
+            inputWaveHeight = 5*[3.0])
             
 
-    # Send message to DDS for DDS-FMU to receive as an output
+    writer.write(msg)
 
-    dw.write(msg)
-# DDS Functions (STOP) --------------------------------------------------
-
-
-
-
-# Variables for DDS Reading
-data = 0
-
-# Variable for DDS communication
-waitPeriod = 0.5
-
-# Variables that change over time
-variableChanging = 0.0
 
 if __name__ == "__main__":
     while True:
-        data = read_data()
+        # data = read_data()
         publish_data()
         
